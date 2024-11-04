9c0ce199b9a5a1c7e514c09e1e397326cfa426f0
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-11-04 16:49:41 -0400

    feat(app): add secure routing and implement chats and status pages

--- app/src/app/secure/secure.routes.ts
+++ app/src/app/secure/secure.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const secureRoutes: Routes = [];
+export const secureRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'chats',
+    pathMatch: 'full',
+  },
+  {
+    path: 'chats',
+    loadComponent: () => import('./pages/chats-page/chats-page.component').then(c => c.ChatsPageComponent),
+  },
+  {
+    path: 'status',
+    loadComponent: () => import('./pages/status-page/status-page.component').then(c => c.StatusPageComponent),
+  },
+];
