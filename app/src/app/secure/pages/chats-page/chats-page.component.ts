ea0bb412f101b44a48af01b0b97d16470a50f34e
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-11-04 16:57:53 -0400

    feat(app): implement dynamic chat previews in chats page using mock data

--- app/src/app/secure/pages/chats-page/chats-page.component.ts
+++ app/src/app/secure/pages/chats-page/chats-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ChatPreviewComponent } from '../../components/chat-preview/chat-preview.component';
+import { ChatPreview } from '../../../core/interfaces/chat-preview.interface';
+import { chats } from '../../../core/mocks/chats.mock';
 
 @Component({
   selector: 'app-chats-page',
@@ -12,4 +14,10 @@ import { ChatPreviewComponent } from '../../components/chat-preview/chat-preview
     }
   `,
 })
-export class ChatsPageComponent {}
+export class ChatsPageComponent {
+  public chats: ChatPreview[];
+
+  constructor() {
+    this.chats = chats;
+  }
+}
