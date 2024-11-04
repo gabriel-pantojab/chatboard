59d2be03d050aef9335581add970784c73094708
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-11-04 16:50:42 -0400

    feat(app): add NgOptimizedImage import to sidebar component and include avatar image asset

--- app/src/app/secure/components/sidebar/sidebar.component.ts
+++ app/src/app/secure/components/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, NgOptimizedImage],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
   host: {
