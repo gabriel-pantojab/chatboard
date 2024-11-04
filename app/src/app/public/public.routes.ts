0df25bf06fb6ee2639b6e2174d43ad88be040557
Gabriel Bustamante <gabrieljuniorpb990@gmail.com>
2024-11-04 14:37:09 -0400

    feat(app): add home page component with routing and update global styles

--- app/src/app/public/public.routes.ts
+++ app/src/app/public/public.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const publicRoutes: Routes = [];
+export const publicRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home-page/home-page.component').then(c => c.HomePageComponent),
+  },
+];
